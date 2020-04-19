e6f7157d91ce5113a0c8d81e35dac37220297e70
esotericpig <esotericpig@users.noreply.github.com>
2020-04-19 18:01:16 +0800

    Initial commit

---
+++ lib/attr_bool/version.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+# frozen_string_literal: true
+
+#--
+# This file is part of attr_bool.
+# Copyright (c) 2020 Jonathan Bradley Whited (@esotericpig)
+# 
+# attr_bool is free software: you can redistribute it and/or modify it under
+# the terms of the MIT License.
+# 
+# You should have received a copy of the MIT License along with attr_bool.
+# If not, see <https://choosealicense.com/licenses/mit/>.
+#++
+
+
+module AttrBool
+  VERSION = '0.1.0'
+end

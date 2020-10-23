3200325a267d1589123e429bb6853c27fc70d5ab
esotericpig <esotericpig@users.noreply.github.com>
2020-10-23 17:05:09 +0800

    Initial commit of TestHelper

---
+++ test/test_helper.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+# frozen_string_literal: true
+
+#--
+# This file is part of AttrBool.
+# Copyright (c) 2020 Jonathan Bradley Whited (@esotericpig)
+# 
+# AttrBool is free software: you can redistribute it and/or modify it under
+# the terms of the MIT License.
+# 
+# You should have received a copy of the MIT License along with AttrBool.
+# If not, see <https://choosealicense.com/licenses/mit/>.
+#++
+
+
+require 'minitest/autorun'
+
+###
+# @author Jonathan Bradley Whited (@esotericpig)
+# @since  0.2.0
+###
+class TestHelper < Minitest::Test
+end
